f4a13a8eb8b35f2b5a7c0a983ff586138172a29b
Luizca95 <Luizca95@hotmail.com>
2019-05-23 22:30:53 -0300

    pequena atualização na página do admin

---
+++ js/menujs.js
@@ -0,0 +1,10 @@
+<!--função para ativar animação do menu-->
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
