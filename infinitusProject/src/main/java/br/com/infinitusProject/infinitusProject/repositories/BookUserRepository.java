5719ec19899c4252f0452f8b9c80f3706e578013
mistless <lari3573@outlook.com>
2019-06-01 23:48:16 -0300

    Revert "Adicionando novos campos na tela myBooks"
    This reverts commit 36a26893040e83ecec8f799afdc4d0617b855493.

--- infinitusProject/src/main/java/br/com/infinitusProject/infinitusProject/repositories/BookUserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.infinitusProject.infinitusProject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.infinitusProject.infinitusProject.models.BookUser;
-
-public interface BookUserRepository extends JpaRepository<BookUser, Long>{
-
-}
