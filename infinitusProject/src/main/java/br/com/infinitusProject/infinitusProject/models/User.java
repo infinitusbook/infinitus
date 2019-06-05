7d0c0ca72c642c6119fc98c9d50abab7c1c0256e
Luiz Carlos Alves da Silva Filho <luiz.filho@easynvest.com.br>
2019-06-05 14:03:13 -0300

    Remove construtor de usuario na models user para testes no azure

--- infinitusProject/src/main/java/br/com/infinitusProject/infinitusProject/models/User.java
+++ infinitusProject/src/main/java/br/com/infinitusProject/infinitusProject/models/User.java
@@ -44,7 +44,7 @@ public class User {
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;                    
 
-    public User() {}
+    //public User() {}
     
 	public Long getId() {
 		return id;
