a1a53c307bdd5d7a60381916eca654e176f39e1e
Aluno <Aluno@Digital.net>
2019-06-19 21:36:26 -0300

    add varias coisas

--- infinitusProject/src/main/resources/static/js/js_modal.js
+++ infinitusProject/src/main/resources/static/js/js_modal.js
@@ -1,16 +1,15 @@
-
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
 }
+
+document.getElementById("defaultOpen").click();
